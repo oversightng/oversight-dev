5fe3aa7f41af41fdc32641e779ddecb2d496928c
mrabalogu <chineduabalogu@yahoo.com>
2018-03-03 15:18:07 +0100

    admin page added

--- src/components/profile.js
+++ src/components/profile.js
@@ -17,8 +17,8 @@ class Profile extends React.Component{
         <span><img alt="logo thumbnail" src="https://i.imgur.com/smX5Xaw.png" className="logo-thumb" /></span>
         <div style={styles.topnavIcons} className="float-left">
           <SplitButton id="23" title={localStorage.getItem('email')}>
-            <MenuItem eventKey="1" href="/admin">Admin Page</MenuItem>
-            <MenuItem eventKey="2" href="/all">All Politicians</MenuItem>
+            <MenuItem eventKey="1" href="/oversight-dev/admin">Admin Page</MenuItem>
+            <MenuItem eventKey="2" href="/oversight-dev/all">All Politicians</MenuItem>
             <MenuItem divider />
             <MenuItem eventKey="4" onClick={this.logout.bind(this)}>Logout</MenuItem>
           </SplitButton>
